e874e4cc2c2b1c0615745c989696d29537cfb408
mariadelaorden <mariadelaorden2005@hotmail.com>
2024-10-25 09:47:44 +0200

    nuevo arbolito

--- src/ArbolHuff.java
+++ src/ArbolHuff.java
@@ -1,8 +1,7 @@
 abstract class ArbolHuff
-    // Devuelve el peso de un ArbolHuf.
+        // Devuelve el peso de un ArbolHuf.
     def peso: Int = this match
             case HojaHuff(_,p) => p
             case RamaHuff(nodoIzq,nodoDcha) => nodoIzq.peso + nodoDcha.peso
-
 case class RamaHuff(nodoIzq: ArbolHuff, nodoDcha: ArbolHuff) extends ArbolHuffman
 case class HojaHuff(caracter: Char, peso: Int) extends ArbolHuffman
