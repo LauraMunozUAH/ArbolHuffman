07e7dc7419e2f501bd0c6da0d41ab005cba4171e
LauraMunozUAH <l.m.centenera@gmail.com>
2024-10-25 10:18:29 +0200

    prueba

--- src/ArbolHuff.scala
+++ src/ArbolHuff.scala
@@ -2,7 +2,12 @@ abstract class ArbolHuff {
   def pesoArbol: Int = this match
     case HojaHuff(_, p) => p
     case RamaHuff(nodoIzq, nodoDcha) => nodoIzq.pesoArbol + nodoDcha.pesoArbol
-}    
-    
+
+  //Función que devuelve la lista de caracteres de un ArbolHuff
+  def caracteres:List[Char]= this match
+    case HojaHuff(c, _) => List(c)
+    case RamaHuff(nodoIzq, nodoDcha) => nodoIzq.caracteres ++ nodoDcha.caracteres
+}
+
 case class RamaHuff(nodoIzq:ArbolHuff, nodoDcha: ArbolHuff) extends ArbolHuff
 case class HojaHuff(caracter:Char, peso: Int) extends ArbolHuff
